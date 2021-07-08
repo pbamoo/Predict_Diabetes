b8c4a4b1e6c00eb03a3099de0e44de07d0421964
pbamoo <71721337+pbamoo@users.noreply.github.com>
2021-07-08 15:20:07 +0200

    Final

---
+++ utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+
+def preprocess(df):
+    print('----------------------------------------------')
+    print("Before preprocessing")
+    print("Number of rows with 0 values for each variable")
+    for col in df.columns:
+        missing_rows = df.loc[df[col]==0].shape[0]
+        print(col + ": " + str(missing_rows))
+    print('----------------------------------------------')
+
+    # Replace 0 values with the mean of the existing values
+    for col in df.columns[1:7]:
+        df[col]=df[col].mask(df[col]==0).fillna(df[col].mean())
+
+    print('----------------------------------------------')
+    print("After preprocessing")
+    print("Number of rows with 0 values for each variable")
+    for col in df.columns:
+        missing_rows = df.loc[df[col]==0].shape[0]
+        print(col + ": " + str(missing_rows))
+    print('----------------------------------------------')
+
+    # Standardization
+    df_scaled = preprocessing.scale(df)
+    df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
+    df_scaled['Outcome'] = df['Outcome']
+    df = df_scaled
+    
+
+    return df
